fb5c9438e5fc5ff934f909fd53b857c0cbd831b9
Abdul Alim <abdulalimrakib53@gmail.com>
2021-07-01 02:20:00 +0600

    first commit

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Scss/Header.scss'
-
 const Header = () => {
     return (
         <div className="header">
-            Header
+            <div className="header-container">
+                <p className="spacial-tag">Hello, I'm </p>
+                <h1>Abdul Alim.</h1>
+                <h2>I Help Designers, Small Agencies And Businesses Bring Their Ideas To Life.</h2>
+                <p className="description"></p>
+                <button>Let's Talk</button>
+            </div>
         </div>
     )
 }
