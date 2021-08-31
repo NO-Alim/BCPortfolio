4f82e860f96570e6c3144e0ffb3e156eaead2bb1
Abdul Alim <abdulalimrakib53@gmail.com>
2021-08-31 17:14:03 +0600

    first commit

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -8,6 +8,7 @@ import prettygal from '../img/pretty-gal.png';
 import fitness from '../img/fitness.png'
 import journalist from '../img/jurnalist.png'
 import FoodBlog from '../img/Food-Blog.png'
+import kallas from '../img/kallas.png'
 const img = 'https://cdn.pixabay.com/photo/2017/07/12/08/35/network-2496193__340.jpg'
 const img2 = 'https://cdn.pixabay.com/photo/2021/05/19/06/13/smartphone-6265046__340.jpg'
 const img3 = 'https://cdn.pixabay.com/photo/2017/09/05/10/08/office-2717014__340.jpg'
@@ -19,6 +20,25 @@ const Work = () => {
             <div className="work-container">
                 <h2>work</h2>
                 <div className="main-projects">
+                <div className="top-project">
+                        <h3>1. Kalles.</h3>
+                        <div className="img-container">
+                            <img src={kallas} />
+                            <div className="project-content">
+                                <p>This is a e-commerce site i develope. I create this site use react.js and also fetch data from <a href="https://www.thecocktaildb.com/">www.thecocktaildb.com</a>. In this site special feature is search product, add and deleting from cart, from validation etc.</p>
+                                <div className="tools">
+                                    <span>React.js</span>
+                                    <span>scss</span>
+                                    <span>TheCocktailDB API</span>
+                                </div>
+                                <div className="links">
+                                    <a target='_blink' href='https://github.com/NO-Alim/Kalle_s.git'><FaGithub /></a>
+                                    <a target='_blink' href='https://awesome-curie-7b522f.netlify.app/'><FaExternalLinkAlt /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="top-project">
                         <h3>1. FoodCommerce.</h3>
                         <div className="img-container">
